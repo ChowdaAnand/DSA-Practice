e845fe343a0fb06cc8fcff457f1dd4494ecc013a
ChowdaAnand <chowdaanand504@gmail.com>
2026-09-23 22:06:49 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (55.75%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPerfectSquare(self, num):
+        low=0
+        high=num 
+        while(low<=high):
+            mid=(low+high)//2
+            if mid*mid==num:
+                return True
+            elif mid*mid<num:
+                low=mid+1
+            else:
+                high=mid-1
+        return False
